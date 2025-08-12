73337231222f09d941c93de0ca5433cfd86387af
Aimanmirza2007 <sm501623@gmail.com>
2025-08-13 01:18:06 +0500

    update

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -14,7 +14,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/login`,
+      `${process.env.NEXT_PUBLIC_ATLAS_URL}/api/auth/login`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
